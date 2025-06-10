c974fcea87d5a41aa8055b2cfd8238c8317c3248
muhammadasad878 <muhammadasad4041@gmail.com>
2025-06-10 19:35:55 +0500

    skills text color change

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,6 +1,9 @@
 import { Code, Database, Server, Globe } from 'lucide-react';
 import { skills } from '@/data/Skills';
 
+
+
+
 const Skills = () => (
   <section id="skills" className="py-20 bg-gray-50">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
